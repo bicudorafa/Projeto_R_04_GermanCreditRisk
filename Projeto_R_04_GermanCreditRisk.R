ae5591560ffc4b9221e990fc7b75277ef4ad7479
Rafael Bicudo <rafaelbicudo.r@outlook.com>
2018-06-23 00:15:57 -0300

    Definicao das 1a operacoes de data munging e inicio do processo de feature selection

--- Projeto_R_04_GermanCreditRisk.R
+++ Projeto_R_04_GermanCreditRisk.R
@@ -12,7 +12,53 @@ names(Credit) <- c('CheckingAcctStat', 'Duration', 'CreditHistory', 'Purpose', '
                    'OtherDetorsGuarantors', 'PresentResidenceTime', 'Property', 'Age', 
                    'OtherInstallments', 'Housing', 'ExistingCreditsAtBank', 'Job', 'NumberDependents', 
                    'Telephone', 'ForeignWorker', 'CreditStatus')
+# Analise do dataframe
+str(Credit)
+## Data Munging
 
-# Fatorizando features obtidas como integers
-Credit[, 'NumberDependents'] <- factor(Credit[, 'NumberDependents'])
+# Definicao variavel de interesse 
 Credit[, 'CreditStatus'] <- factor(as.character(Credit[, 'CreditStatus']), labels = c('Good', 'Bad'))
+
+# Funcao para automatizar "fatorizacao" das variaveis
+to.factor <- function(df, features) {
+  for (feature in features) {
+    df[[feature]] <- as.factor(df[[feature]])
+  }
+  return(df)
+}
+
+# Criacao do string vector das variaveis a  serem fatorizadas e sua fatorizacao
+categorical_vars <- c('CheckingAcctStat', 'CreditHistory', 'Purpose', 
+                     'SavingsBonds', 'Employment', 'InstallmentRatePecnt', 'SexAndStatus', 
+                     'OtherDetorsGuarantors', 'PresentResidenceTime', 'Property',  
+                     'OtherInstallments', 'Housing', 'ExistingCreditsAtBank', 'Job', 'NumberDependents', 
+                     'Telephone', 'ForeignWorker', 'CreditStatus')
+
+Credit <- to.factor(Credit, categorical_vars)
+
+
+## Preparacao dos dados para modelo preditivo
+
+# Funcao para automatizar normalizacao
+scale.features <- function(df, variables){
+  for (variable in variables){
+    df[[variable]] <- scale(df[[variable]], center=T, scale=T)
+  }
+  return(df)
+}
+
+# Normalizacao das variaveis
+numeric_vars <- c("Duration", "Age", "CreditAmount")
+scale_Credit <- scale.features(Credit, numeric_vars)
+
+# Separacao dos Sets de Treino e Teste
+set.seed(69)
+sample <- caTools::sample.split(scale_Credit$CreditStatus, SplitRatio = 0.70)
+train_sample <- as.data.frame(subset(Credit, sample == T))
+test_sample <- as.data.frame(subset(Credit, sample == F))
+
+## Feature Selection
+
+# Carregando pacotes necessarios
+library(caret) 
+library(randomForest) 
